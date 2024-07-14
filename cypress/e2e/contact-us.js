f2f6185690eaed3347a4b99b76fe43cec72d00fd
NadyaPod <nadegda.podyacheva@gmail.com>
2024-07-14 12:55:41 +0700

    feat: contact tests

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,11 +1,18 @@
 describe('Test Contact Us form via WebdriverUni', () => {
   it('Should be able to submit', () => {
-    cy.visit('https://webdriveruniversity.com/')
-    cy.get('#contact-us > .thumbnail').click()
+    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    // cy.get('#contact-us > .thumbnail').click()
+    cy.get('[name="first_name"]').type('Nadegda')
+    cy.get('[name="last_name"]').type('Nadegda')
+    cy.get('[name="email"]').type('nadegda@nadegda.com')
+    cy.get('textarea.feedback-input').type('Nadegda')
+    cy.get('[type="submit"]').click( )
   });
-
   it('Should not be able to submit', () => {
-    
-  });
+    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    cy.get('[name="first_name"]').type('Nadegda')
+    cy.get('[name="last_name"]').type('Nadegda')
+    cy.get('[type="submit"]').click( )
+  })
 })
 
