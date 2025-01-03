66b72a3062fe41cc33bd2cc6c41c68b68fe9d872
NadyaPod <nadegda.podyacheva@gmail.com>
2025-01-03 14:58:47 +0700

    feat: xpath support

--- cypress/e2e/test-store/contact-us.js
+++ cypress/e2e/test-store/contact-us.js
@@ -2,7 +2,8 @@ describe('Test Contact Us form via Test-Store', () => {
   it('Should be able to submit', () => {
     cy.visit('https://automationteststore.com/')
     // cy.get('#contact-us > .thumbnail').click()
-    cy.get('.info_links_footer > :nth-child(5) > a').click();
+    // cy.get('.info_links_footer > :nth-child(5) > a').click();
+    cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get('#ContactUsFrm_first_name').type('Nadegda');
     cy.get('#ContactUsFrm_email').type('nadegda@nadegda.com');
     cy.get('#ContactUsFrm_enquiry').type('Nadegda');
