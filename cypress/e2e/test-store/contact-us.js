ef0a4492e5a2d8acef470cdaaae56e23c0023b3b
NadyaPod <nadegda.podyacheva@gmail.com>
2025-01-03 15:58:05 +0700

    feat: assert

--- cypress/e2e/test-store/contact-us.js
+++ cypress/e2e/test-store/contact-us.js
@@ -2,12 +2,14 @@ describe('Test Contact Us form via Test-Store', () => {
   it('Should be able to submit', () => {
     cy.visit('https://automationteststore.com/')
     // cy.get('#contact-us > .thumbnail').click()
-    // cy.get('.info_links_footer > :nth-child(5) > a').click();
-    cy.xpath("//a[contains(@href, 'contact')]").click();
+    // cy.get('.info_links_footer > :nth-child(5) > a').click();\
+    cy.get("a[href$='contact']").click();
+    // cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get('#ContactUsFrm_first_name').type('Nadegda');
     cy.get('#ContactUsFrm_email').type('nadegda@nadegda.com');
     cy.get('#ContactUsFrm_enquiry').type('Nadegda');
-    cy.get('.col-md-6 > .btn').click();
+    cy.get("button[title='Submit']").click();
+    cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!');
   });
 })
 
