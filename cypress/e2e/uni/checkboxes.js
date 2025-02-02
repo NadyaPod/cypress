c83cd025d9e2ec83041d46f8bbf777ce453ce22e
NadyaPod <nadegda.podyacheva@gmail.com>
2025-02-02 14:57:28 +0700

    feat: checkboxes

---
+++ cypress/e2e/uni/checkboxes.js
@@ -0,0 +1,15 @@
+describe('Checkboxes tests', () => {
+  it('Verify checkboxes', () => {
+    cy.visit('https://webdriveruniversity.com');
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true});
+    cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked');
+  });
+
+  it('Verify checkbox is unchecked', () => {
+    cy.visit('https://webdriveruniversity.com');
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true});
+    cy.get('input[type="checkbox"]')
+      .uncheck(['option-3'])
+      .should('not.be.checked')
+  });
+})
