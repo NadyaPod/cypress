51e033639ef986d3c4877f65ba29de9c4a609702
NadyaPod <nadegda.podyacheva@gmail.com>
2025-02-04 20:20:34 +0700

    feat: multiple checkboxes

--- cypress/e2e/uni/checkboxes.js
+++ cypress/e2e/uni/checkboxes.js
@@ -12,4 +12,11 @@ describe('Checkboxes tests', () => {
       .uncheck(['option-3'])
       .should('not.be.checked')
   });
+  it('Check multiple checkboxes', () => {
+    cy.visit('https://webdriveruniversity.com');
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true});
+    cy.get('input[type="checkbox"]')
+      .check(['option-1', 'option-2', 'option-3', 'option-4'])
+      .should('be.checked')
+  });
 })
